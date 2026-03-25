e9e434c7abf72134b9fd0ac18cf3d215a4939928
chr-poke <chircuandrei28@gmail.com>
2026-03-25 05:14:32 +0200

    Refined pokemon class, added type and move classes, created objects.

--- main.cpp
+++ main.cpp
@@ -1,52 +1,141 @@
 #include <iostream>
 #include <string>
 #include <random>
-#include <array>
 #include <vector>
 #include <cmath>
-#include "include/Example.h"
-// This also works if you do not want `include/`, but some editors might not like it
-// #include "Example.h"
+#include <set>
+
+class pokeType {
+private:
+    int id;
+    std::string name;
+    std::vector<float> attack;
+    std::vector<float> defense;
+
+public:
+    pokeType() {
+        this->id = 0;
+        this->name = "None";
+        this->attack = {1, 1, 1, 1, 1, 1, 1};
+        this->defense = {1, 1, 1, 1, 1, 1, 1};
+    }
+
+    pokeType(const int id, const std::string& name,
+         const std::vector<float>& attack,
+         const std::vector<float>& defense) {
+        this->id = id;
+        this->name = name;
+        this->attack = attack;
+        this->defense = defense;
+    }
+
+    pokeType(const pokeType& other) {
+        this->id = other.id;
+        this->name = other.name;
+        this->attack = other.attack;
+        this->defense = other.defense;
+    }
+
+    pokeType& operator=(const pokeType& other) = default;
+
+    [[nodiscard]] float getAttack(int i) const {
+        return attack[i];
+    }
+    [[nodiscard]] float getDefense(int i) const {
+        return defense[i];
+    }
+};
+
+pokeType noneType(0, "None",
+          {1, 1, 1, 1, 1, 1, 1},
+          {1, 1, 1, 1, 1, 1, 1});
+pokeType normalType(1, "Normal",
+          {1, 1, 1, 1, 1, 1, 1},
+          {1, 1, 1, 1, 1, 1, 1});
+pokeType fireType(2, "Fire",
+          {1, 1, 0.5, 0.5, 2, 2, 1},
+          {1, 1, 0.5, 2, 0.5, 0.5, 1});
+pokeType waterType(3, "Water",
+          {1, 1, 2, 0.5, 0.5, 1, 1},
+          {1, 1, 0.5, 0.5, 2, 1, 1});
+pokeType grassType(4, "Grass",
+          {1, 1, 0.5, 2, 0.5, 0.5, 1},
+          {1, 1, 2, 0.5, 0.5, 2, 1});
+pokeType bugType(5, "Bug",
+          {1, 1, 0.5, 1, 2, 1, 2},
+          {1, 1, 2, 1, 0.5, 1, 1});
+pokeType darkType(6, "Dark",
+          {1, 1, 1, 1, 1, 1, 0.5},
+           {1, 1, 1, 1, 1, 2, 0.5});
 
 class move {
+private:
+    pokeType type;
+    std::string name;
+    int power;
 
+public:
+    move() {
+        this->type = noneType;
+        this->name = "None";
+        this->power = 0;
+    }
+
+    move(const pokeType& type, const int power, const std::string& name) {
+        this->type = type;
+        this->name = name;
+        this->power = power;
+    }
+
+    [[nodiscard]] int getPower() const {
+        return power;
+    }
+    [[nodiscard]] pokeType getType() const {
+        return type;
+    }
 };
+
+move leerMove(normalType, 0, "Leer");
+move poundMove(normalType, 40, "Pound");
+move absorbMove(grassType, 20, "Absorb");
+move quick_attackMove(normalType, 40, "Quick Attack");
+move growlMove(normalType, 0, "Growl");
+move scratchMove(normalType, 40, "Scratch");
+move focus_energyMove(normalType, 0, "Focus Energy");
+move emberMove(fireType, 40, "Ember");
+move tackleMove(normalType, 35, "Tackle");
+move mud_slapMove(normalType, 20, "Mud Slap");
+move water_gunMove(waterType, 40, "Water Gun");
+move howlMove(normalType, 0, "Howl");
+move sand_attackMove(normalType, 0, "Sand Attack");
+move biteMove(darkType, 40, "Bite");
 
 class pokemon {
 private:
     int pokedexNumber; // Hoenn Pokedex
-    int level;
-    int xp;
+    int full_hp;
     int hp;
-    int type1;
-    int type2;
-    std::string statusCondition;
+    int attack;
+    int defense;
+    pokeType type1;
+    pokeType type2;
+    std::vector<move> moveset;
     std::string name;
-public:
-    pokemon() {
-        // wild encounter
+    int level;
+    double xp;
+    int status;
+    // 0 - wild pokemon
+    // 1 - team member
+    // 2 - rejected starter
+
+    static float getRandom() {
+        static std::random_device rd;
+        static std::mt19937 gen(rd());
+        std::uniform_real_distribution<float> dist(217.0f / 255.0f, 1.0f);
+        return dist(gen);
     }
 
-    pokemon(int pokedexNumber, int level) {
-        this->pokedexNumber = pokedexNumber;
-        this->level = level;
-        this->xp = pow(level, 3);
-        // hp din db
-        // types din db
-        this->statusCondition = "None";
-        switch (pokedexNumber) {
-            case 1:
-                this->name = "Treecko";
-                break;
-            case 4:
-                this->name = "Torchic";
-                break;
-            case 7:
-                this->name = "Mudkip";
-                break;
-            default:
-                this->name = "Unknown";
-        }
+    void setNickname() {
         std::string input;
         bool dialog = true;
         while (dialog) {
@@ -62,62 +151,85 @@ public:
         }
     }
 
-    std::string getNume() {
-        return this->name;
+public:
+    pokemon(const int pokedexNumber, const int full_hp,
+                const int attack, const int defense,
+                const pokeType& type1, const pokeType& type2,
+                const std::vector<move>& moveset, const std::string& name,
+                const int level) {
+        this->pokedexNumber = pokedexNumber;
+        this->full_hp = full_hp;
+        this->hp = full_hp;
+        this->attack = attack;
+        this->defense = defense;
+        this->type1 = type1;
+        this->type2 = type2;
+        this->moveset = moveset;
+        this->name = name;
+        this->level = level;
+        this->xp = pow(level, 3);
+        this->status = 0;
+    }
+
+    ~pokemon() {
+        if (status == 0)
+            std::cout << "Wild " + name + " fainted!\n";
+        else if (status == 1)
+            std::cout << name + " was released.\n"
+                      << "Bye-bye, " + name + "!\n";
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const pokemon& obj) {
+        os << obj.name << " (Level " << obj.level << ")\n"
+           << "HP: " << obj.hp << "/" << obj.full_hp << "\n";
+        return os;
+    }
+
+    void changeStatus(int newStatus) {
+        status = newStatus;
+        if (newStatus == 1)
+            setNickname();
+    }
+
+    void xpGain(int b, int l) {
+        // b = xp of the defeated Pokemon
+        // l = level of the defeated Pokemon
+        xp += b * l / 7;
+        if (xp >= pow(level + 1, 3)) {
+            level++;
+            attack++;
+            defense++;
+            hp += 2;
+        }
+    }
+
+    [[nodiscard]] int damage(const int p, const int d, const float t1, const float t2) const {
+        const float r = getRandom();
+        return std::floor(
+            (static_cast<float>(((2 * level) / 5 + 2) * p * attack) / static_cast<float>(d)) / 50.0 * t1 * t2 * r);
     }
 };
 
+pokemon treecko(1, 19, 10, 9,
+            grassType, noneType,
+            {leerMove, poundMove, absorbMove},
+            "Treecko", 5);
+pokemon torchic(4, 20, 11, 9,
+            fireType, noneType,
+            {growlMove, scratchMove, focus_energyMove},
+            "Torchic", 5);
+pokemon mudkip(7, 20, 12, 10,
+            waterType, noneType,
+            {growlMove, tackleMove, mud_slapMove},
+            "Mudkip", 5);
+pokemon poochyena(10, 13, 7, 6,
+            darkType, noneType,
+            {tackleMove, howlMove, sand_attackMove},
+            "Poochyena", 2);
+
 int main() {
-    /*
-    std::cout << "Hello, world!\n";
-    Example e1;
-    e1.g();
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
-    }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
-    }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
-    return 0;
-    */
     // *** STARTER SELECTION ***
+
     int selectedStarter = 0;
     bool starterWasChosen = false;
     std::cout << "Professor Birch is in trouble! Release a Pokemon and rescue him!\n"
@@ -151,7 +263,29 @@ int main() {
         else
             std::cout << "Invalid input!\n";
     }
-    pokemon starter((selectedStarter - 1) * 3 + 1, 5);
+
+    switch (selectedStarter) {
+        case 1:
+            treecko.changeStatus(1);
+            torchic.changeStatus(2);
+            mudkip.changeStatus(2);
+            std::cout << treecko << std::endl;
+            break;
+        case 2:
+            treecko.changeStatus(2);
+            torchic.changeStatus(1);
+            mudkip.changeStatus(2);
+            std::cout << torchic << std::endl;
+            break;
+        case 3:
+            treecko.changeStatus(2);
+            torchic.changeStatus(2);
+            mudkip.changeStatus(1);
+            std::cout << mudkip << std::endl;
+            break;
+        default:
+            std::cout << "Invalid input!\n";
+    }
 
     return 0;
 }
