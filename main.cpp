a515904197d11b90c8d827b06d29365634c0b5d2
chr-poke <chircuandrei28@gmail.com>
2026-03-25 14:34:12 +0200

    Added battles, tweaked classes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <random>
+#include <utility>
 #include <vector>
 #include <cmath>
 #include <set>
@@ -20,14 +21,10 @@ public:
         this->defense = {1, 1, 1, 1, 1, 1, 1};
     }
 
-    pokeType(const int id, const std::string& name,
-         const std::vector<float>& attack,
-         const std::vector<float>& defense) {
-        this->id = id;
-        this->name = name;
-        this->attack = attack;
-        this->defense = defense;
-    }
+    pokeType(const int id_, std::string name_,
+         const std::vector<float>& attack_,
+         const std::vector<float>& defense_) : id{id_}, name{std::move(name_)},
+                attack{attack_}, defense{defense_} {}
 
     pokeType(const pokeType& other) {
         this->id = other.id;
@@ -38,11 +35,22 @@ public:
 
     pokeType& operator=(const pokeType& other) = default;
 
+    friend std::ostream& operator<<(std::ostream& os, const pokeType& obj) {
+        os << "The " << obj.name << " Type has the id " << obj.id << ".\n";
+        return os;
+    }
+
     [[nodiscard]] float getAttack(int i) const {
         return attack[i];
     }
     [[nodiscard]] float getDefense(int i) const {
         return defense[i];
+    }
+    [[nodiscard]] std::string getName() const {
+        return name;
+    }
+    [[nodiscard]] int getID() const {
+        return id;
     }
 };
 
@@ -81,10 +89,13 @@ public:
         this->power = 0;
     }
 
-    move(const pokeType& type, const int power, const std::string& name) {
-        this->type = type;
-        this->name = name;
-        this->power = power;
+    move(const pokeType& type_, const int power_, std::string name_) :
+        type{type_}, name{std::move(name_)}, power{power_} {}
+
+    friend std::ostream& operator<<(std::ostream& os, const move& obj) {
+        os << obj.name << " is a " << obj.type.getName()
+           << " Type move and it has " << obj.power << " power.\n";
+        return os;
     }
 
     [[nodiscard]] int getPower() const {
@@ -92,6 +103,9 @@ public:
     }
     [[nodiscard]] pokeType getType() const {
         return type;
+    }
+    [[nodiscard]] std::string getName() const {
+        return name;
     }
 };
 
@@ -152,29 +166,56 @@ private:
     }
 
 public:
-    pokemon(const int pokedexNumber, const int full_hp,
-                const int attack, const int defense,
-                const pokeType& type1, const pokeType& type2,
-                const std::vector<move>& moveset, const std::string& name,
-                const int level) {
-        this->pokedexNumber = pokedexNumber;
-        this->full_hp = full_hp;
-        this->hp = full_hp;
-        this->attack = attack;
-        this->defense = defense;
-        this->type1 = type1;
-        this->type2 = type2;
-        this->moveset = moveset;
-        this->name = name;
-        this->level = level;
-        this->xp = pow(level, 3);
-        this->status = 0;
+    pokemon() {
+        this->pokedexNumber = 0;
+        this->full_hp = 0;
+        this->hp = 0;
+        this->attack = 0;
+        this->defense = 0;
+        this->type1 = noneType;
+        this->type2 = noneType;
+        this->moveset = {};
+        this->name = "Unknown";
+        this->level = 0;
+        this->xp = 0;
+        this->status = 2;
     }
 
-    ~pokemon() {
+    pokemon(const int pokedexNumber_, const int full_hp_,
+            const int attack_, const int defense_,
+            const pokeType& type1_, const pokeType& type2_,
+            const std::vector<move>& moveset_, std::string  name_,
+            const int level_, const int status_) :
+                pokedexNumber{pokedexNumber_}, full_hp{full_hp_},
+                attack{attack_}, defense{defense_},
+                type1{type1_}, type2{type2_},
+                moveset{moveset_}, name{std::move(name_)},
+                level{level_}, status{status_} {
+        this->hp = full_hp;
+        this->xp = pow(level, 3);
         if (status == 0)
-            std::cout << "Wild " + name + " fainted!\n";
-        else if (status == 1)
+            std::cout << "Wild " << name << " appeared!\n";
+    }
+
+    pokemon(const pokemon& other) {
+        this->pokedexNumber = other.pokedexNumber;
+        this->full_hp = other.full_hp;
+        this->hp = other.full_hp;
+        this->attack = other.attack;
+        this->defense = other.defense;
+        this->type1 = other.type1;
+        this->type2 = other.type2;
+        this->moveset = other.moveset;
+        this->name = other.name;
+        this->level = other.level;
+        this->xp = pow(other.level, 3);
+        this->status = other.status;
+    }
+
+    pokemon& operator=(const pokemon& other) = default;
+
+    ~pokemon() {
+        if (status == 1)
             std::cout << name + " was released.\n"
                       << "Bye-bye, " + name + "!\n";
     }
@@ -191,7 +232,7 @@ public:
             setNickname();
     }
 
-    void xpGain(int b, int l) {
+    int xpGain(double b, int l) {
         // b = xp of the defeated Pokemon
         // l = level of the defeated Pokemon
         xp += b * l / 7;
@@ -199,33 +240,86 @@ public:
             level++;
             attack++;
             defense++;
-            hp += 2;
+            full_hp += 2;
         }
+        return b * l / 7;
+    }
+
+    void takeDamage(int damage) {
+        hp -= damage;
+        if (hp < 0)
+            hp = 0;
     }
 
     [[nodiscard]] int damage(const int p, const int d, const float t1, const float t2) const {
+        // p = move power
+        // d = defense of target
+        // t1 = target's 1st type effectiveness
+        // t2 = target's 2nd type effectiveness
+        // r = random number between 217/255 and 1
         const float r = getRandom();
         return std::floor(
             (static_cast<float>(((2 * level) / 5 + 2) * p * attack) / static_cast<float>(d)) / 50.0 * t1 * t2 * r);
+    }
+
+    move selMove() {
+        std::string input;
+        std::cout << "| ";
+        for (int i = 0; i < moveset.size(); i++)
+            std::cout << i + 1 << ". " << moveset[i].getName() << " | ";
+        std::cout << std::endl << std::endl;
+        std::cout << "Use a move by typing '1'-'"
+                  << std::to_string(moveset.size()) << "'.\n";
+        std::cout << "Type 'help' to learn more about " << name << "'s moves.\n";
+        while (std::cin >> input) {
+            if (input == "help")
+                for (int i = 0; i < moveset.size(); i++)
+                    std::cout << i + 1 << ". " << moveset[i];
+            else if (stol(input) >= 1 && stol(input) <= moveset.size())
+                return moveset[stoi(input) - 1];
+            else
+                std::cout << "Invalid input!\n";
+        }
+    }
+
+    [[nodiscard]] std::string getName() const {
+        return name;
+    }
+    [[nodiscard]] int getHP() const {
+        return hp;
+    }
+    [[nodiscard]] int getDefense() const {
+        return defense;
+    }
+    [[nodiscard]] pokeType getType1() const {
+        return type1;
+    }
+    [[nodiscard]] pokeType getType2() const {
+        return type2;
+    }
+    [[nodiscard]] std::vector<move> getMoveset() const {
+        return moveset;
+    }
+    [[nodiscard]] double getXP() const {
+        return xp;
+    }
+    [[nodiscard]] int getLevel() const {
+        return level;
     }
 };
 
 pokemon treecko(1, 19, 10, 9,
             grassType, noneType,
             {leerMove, poundMove, absorbMove},
-            "Treecko", 5);
+            "Treecko", 5, 2);
 pokemon torchic(4, 20, 11, 9,
             fireType, noneType,
             {growlMove, scratchMove, focus_energyMove},
-            "Torchic", 5);
+            "Torchic", 5, 2);
 pokemon mudkip(7, 20, 12, 10,
             waterType, noneType,
             {growlMove, tackleMove, mud_slapMove},
-            "Mudkip", 5);
-pokemon poochyena(10, 13, 7, 6,
-            darkType, noneType,
-            {tackleMove, howlMove, sand_attackMove},
-            "Poochyena", 2);
+            "Mudkip", 5, 2);
 
 int main() {
     // *** STARTER SELECTION ***
@@ -264,27 +358,75 @@ int main() {
             std::cout << "Invalid input!\n";
     }
 
+    pokemon starter;
     switch (selectedStarter) {
         case 1:
-            treecko.changeStatus(1);
-            torchic.changeStatus(2);
-            mudkip.changeStatus(2);
-            std::cout << treecko << std::endl;
+            starter = treecko;
             break;
         case 2:
-            treecko.changeStatus(2);
-            torchic.changeStatus(1);
-            mudkip.changeStatus(2);
-            std::cout << torchic << std::endl;
+            starter = torchic;
             break;
         case 3:
-            treecko.changeStatus(2);
-            torchic.changeStatus(2);
-            mudkip.changeStatus(1);
-            std::cout << mudkip << std::endl;
+            starter = mudkip;
             break;
         default:
-            std::cout << "Invalid input!\n";
+            std::cout << "You're not supposed to see this!\n";
+    }
+    starter.changeStatus(1);
+
+    // *** BATTLE SIMULATOR ***
+
+    bool onRoute = true;
+    while (onRoute) {
+        for (int i = 0; i < 1; i++) {
+            std::string input;
+            pokemon poochyena(10, 17, 10, 8,
+                darkType, noneType,
+                {tackleMove, howlMove, sand_attackMove},
+                "Poochyena", 4, 0);
+            std::cout << "Go! " << starter.getName() << "!\n\n";
+
+            while (poochyena.getHP() > 0 && starter.getHP() > 0) {
+                std::cout << poochyena << std::endl << starter;
+                move myMove = starter.selMove();
+                std::cout << "--- " << starter.getName() << " used " << myMove.getName() << "! ---\n";
+                poochyena.takeDamage(starter.damage(
+                    myMove.getPower(), poochyena.getDefense(),
+                    myMove.getType().getAttack(poochyena.getType1().getID()),
+                    myMove.getType().getAttack(poochyena.getType2().getID())));
+                if (poochyena.getHP() <= 0)
+                    break;
+
+                std::cout << std::endl << poochyena << std::endl << starter << std::endl;
+                move oppMove = poochyena.getMoveset()[rand() % poochyena.getMoveset().size()];
+                std::cout << "--- " << poochyena.getName() << " used " << oppMove.getName() << "! ---\n\n";
+                starter.takeDamage(poochyena.damage(
+                    oppMove.getPower(), starter.getDefense(),
+                    oppMove.getType().getAttack(starter.getType1().getID()),
+                    oppMove.getType().getAttack(starter.getType2().getID())));
+            }
+            if (starter.getHP() <= 0) {
+                std::cout << starter.getName() << " fainted!\n\n";
+                std::cout << "You blacked out!\n";
+                onRoute = false;
+            }
+            else {
+                std::cout << "\nWild " << poochyena.getName() << " fainted!\n";
+                std::cout << starter.getName() << " gained "
+                          << starter.xpGain(poochyena.getXP(), poochyena.getLevel())
+                          << " XP.\n\n";
+                std::cout << "Do you want to leave Route 101? Y/N \n";
+                while (std::cin >> input) {
+                    if (input == "Y") {
+                        onRoute = false;
+                        break;
+                    }
+                    if (input == "N")
+                        break;
+                    std::cout << "Invalid input!\n";
+                }
+            }
+        }
     }
 
     return 0;
