aeba323895dd05e376a8c1d7837c941c07a67ea4
chr-poke <chircuandrei28@gmail.com>
2026-05-13 05:56:40 +0300

    increased difficulty

--- src/move.cpp
+++ src/move.cpp
@@ -27,9 +27,8 @@ void move::print(std::ostream& os) const {
 }
 
 void physicalMove::execute(pokemon& attacker, pokemon& defender) {
-    if (attacker.getHP() <= 0) {
+    if (attacker.getHP() <= 0)
         throw battleException(attacker.getName() + " has fainted!");
-    }
     std::cout << "--- " << attacker.getName() << " used " << name << "! ---\n";
     const int dmg = attacker.damage(false, power, defender.getDefense(),
                               type.getAttack(defender.getType1().getID()),
@@ -44,9 +43,8 @@ void physicalMove::print(std::ostream& os) const {
 }
 
 void specialMove::execute(pokemon& attacker, pokemon& defender) {
-    if (attacker.getHP() <= 0) {
+    if (attacker.getHP() <= 0)
         throw battleException(attacker.getName() + " has fainted!");
-    }
     std::cout << "--- " << attacker.getName() << " used " << name << "! ---\n";
     const int dmg = attacker.damage(true, power, defender.getSpDefense(),
                               type.getAttack(defender.getType1().getID()),
@@ -61,9 +59,8 @@ void specialMove::print(std::ostream& os) const {
 }
 
 void statusMove::execute(pokemon& attacker, pokemon& defender) {
-    if (attacker.getHP() <= 0) {
+    if (attacker.getHP() <= 0)
         throw battleException(attacker.getName() + " has fainted!");
-    }
     std::cout << "--- " << attacker.getName() << " used " << name << "! ---\n";
 
     for (int i = 0; i < 8; i++) {
